6ec4709a30560b0972bfc58e3e7942cbb9bdf773
alex-zayats <alex3953898@yandex.ru>
2022-07-17 12:16:27 +0200

    fix: add CORS

--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -1,6 +1,11 @@
 export const formatJSONResponse = (response: any) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
+    body: JSON.stringify(response),
+    headers: {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
   }
 }
