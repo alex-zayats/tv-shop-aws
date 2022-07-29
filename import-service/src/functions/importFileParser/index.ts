00f7b01bb337a7430a0bf8c6d454c9df9366af91
unknown <alex3953898@yandex.ru>
2022-07-29 21:33:03 +0200

    feat: add import service

---
+++ import-service/src/functions/importFileParser/index.ts
@@ -0,0 +1,19 @@
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      s3: {
+        bucket: process.env.IMPORT_S3_BUCKET,
+        event: 's3:ObjectCreated:*',
+        rules: [
+          {
+                prefix: 'uploaded/'
+          }
+        ],
+        existing: true
+      },
+    },
+  ],
+};
